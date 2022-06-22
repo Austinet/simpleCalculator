66d1e5b536a78e80851098c4f9a037d62f499a7e
Austinet <austinetsplendour@gmail.com>
2022-06-22 18:28:49 +0100

    first commit, added the files

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let firstNumber = Number(prompt("ENTER THE FIRST NUMBER"));
+let operator = prompt("ENTER THE OPERATOR +, *, -, /, %")
+let secondNumber = Number(prompt("ENTER THE SECOND NUMBER"));
+
+let result;
+
+if(operator === "+") {
+    result = firstNumber + secondNumber;
+} else if (operator === "-") {
+    result = firstNumber - secondNumber;
+} else if(operator === "*"){
+    result = firstNumber * secondNumber;
+}  else if (operator === "/") {
+    result = firstNumber / secondNumber;
+} else if (operator === "%") {
+    result = firstNumber % secondNumber;
+} else {
+    alert("Invalid operator")
+}
+
+alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
